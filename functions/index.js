71ef26c90c2f378e28e8d817d666a20802b5d666
Kevin Choi <kevinyoungchoi@gmail.com>
2021-04-20 17:02:32 -0400

    uncommented

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ app.post('/cars', newCar)
 app.patch('/cars/:carId', updateCar)
 app.delete('/cars/:carId', deleteCar)
 
-// exports.app = functions.https.onRequest(app)
-app.listen(5001)
+exports.app = functions.https.onRequest(app)
+
 
 
