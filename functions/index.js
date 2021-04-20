0ca7e92cc557034e2434fc43b5a3b3745da0e685
Kevin Choi <kevinyoungchoi@gmail.com>
2021-04-20 11:05:20 -0400

    ** paths for app

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,19 @@
 const functions = require("firebase-functions")
+const express = require('express')
+
+const app = express()
+
+app.get('/test', (req, res) => {
+  res.send('It is working!')
+})
+app.get('/test2', (req, res) => {
+  res.send('It is working AGAIN!')
+})
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!")
 })
+
+exports.app = functions.https.onRequest(app)
 
 
